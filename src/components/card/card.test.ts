74ae9e1722aa7a927f4aec933b65a44be1977eb5
Fabio <fabiodn.798@gmail.com>
2023-02-05 23:03:09 +0100

    Testing card

---
+++ src/components/card/card.test.ts
@@ -0,0 +1,23 @@
+import { screen, fireEvent } from '@testing-library/dom';
+import '@testing-library/jest-dom';
+import { PokeStructure } from '../../models/pokemon';
+import { Card } from '../card/card';
+
+describe('Given Card component', () => {
+  const updateMock = jest.fn();
+  const mockCard: PokeStructure = { id: 1, name: 'Fabio', favorite: false };
+  let element: Card;
+  beforeEach(() => {
+    document.body.innerHTML = '<ul></ul>';
+    element = new Card('ul', mockCard, updateMock);
+  });
+  test('It should be in the document', () => {
+    expect(element).toBeInstanceOf(Card);
+  });
+  test('It render the card in the document', () => {
+    const p = screen.getByText('Fabio');
+    expect(p).toBeInTheDocument();
+    // const span = screen.getByText(mockCard.name);
+    // expect(span).toBeInTheDocument();
+  });
+});
