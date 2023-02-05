b57b84d2aa735a22a5e986fe53967fe7ab98398f
Fabio <fabiodn.798@gmail.com>
2023-02-05 23:03:57 +0100

    Styling header and footer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Card } from './components/card/card';
 import { Cards } from './components/cards/cards';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
 import { PokemonApiRepo } from './services/poke.api.repo';
 
 console.log(new Cards('main', new PokemonApiRepo()));
+new Header('#root');
+new Footer('#root');
